7f4f27cd943e9315a84400e09ab4362742e13aa1
Merwyn Ra <marcoetude@gmail.com>
2023-03-06 14:56:12 +0100

    Création des classes

---
+++ EcoleTln/Classes Métier/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes.ClassesEcole
+{
+    class Contact
+    {
+        protected int matricule;
+        protected string nom;
+        protected int anneeArrivee;
+
+
+        Contact(int matricule, string nom, int anneeArrivee)
+        {
+            this.matricule = matricule;
+            this.nom = nom;
+            this.anneeArrivee = anneeArrivee;
+        }
+    }
+}
---
+++ EcoleTln/Ecole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes.ClassesEcole
+{
+    class Ecole
+    {
+        // Attributs
+        private string nom;
+        private int anneeCreation;
+        private Dictionary<string, string>
+    }
+}
